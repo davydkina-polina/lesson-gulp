ddb61e34d24f42a0db746da03dbb7c0958a337af
davydkina-polina <289199@mail.ru>
2020-03-05 15:26:44 +0300

    feat: added gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,33 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
-const cleanCSS = require('gulp-clean-css');
-const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+// const cleanCSS = require('gulp-clean-css');
+// const rename = require('gulp-rename');
 
 // Static server
-gulp.task('browser-sync', () => {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
 
-gulp.task('minify-css', () => {
-  return gulp.src('styles/*.css')
-    .pipe(cleanCSS({debug: true}, (details) => {
-      console.log(`${details.name}: ${details.stats.originalSize}`);
-      console.log(`${details.name}: ${details.stats.minifiedSize}`);
-    }))
-    .pipe(gulp.dest('dist'));
-});
+function serveSass() {
+  return src('./sass/*.sass')
+    .pipe(sass()) // запускаем sass
+    .pipe(dest(".css"))
+    .pipe(browserSync.stream());
+};
 
-gulp.task('gulp-rename', () => {
-  return gulp.src('styles/*.css')
-    // .pipe(rename('.min.css'));
-    .pipe(rename({ suffix: '.min', prefix : '' }))
-})
+exports.serve = bs;
 
-
-// const {src, dest, watch, series, task} = require('gulp');
-// // const cleanCSS = require('gulp-clean-css');
-// // const jsmin = require('gulp-jsmin');
-// // const rename = require('gulp-rename');
-// const sass = require('gulp-sass');
-// var browserSync = require('browser-sync').create();
-
-// // Static server
-// function bs() {
-//   serveSass();
-//   browserSync.init({
-//       server: {
-//           baseDir: "./src/"
-//       }
-//   });
-//   watch("./src/*.html").on('change', browserSync.reload);
-//   watch("./src/sass/**/*.sass", serveSass);
-//   watch("./src/sass/**/*.scss", serveSass);
-//   watch("./src/js/*.js").on('change', browserSync.reload);
-// };
-
-// function serveSass() {
-//   return src("./src/sass/**/*.sass", "./src/sass/**/*.scss")
-//       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-//       .pipe(dest("./src/css"))
-//       .pipe(browserSync.stream());
-// };
-
-// exports.one = bs;
+// gulp.task('gulp-rename', () => {
+//   return gulp.src('styles/*.css')
+//     // .pipe(rename('.min.css'));
+//     .pipe(rename({ suffix: '.min', prefix : '' }))
+// })
