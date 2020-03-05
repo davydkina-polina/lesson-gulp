39c9dc1e4b3297f1ccb7a2173dd5c87507266cfd
davydkina-polina <289199@mail.ru>
2020-03-06 00:00:47 +0300

    feat: added  sass-files, changed gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -14,13 +14,14 @@ function bs() {
   });
   watch("./*.html").on('change', browserSync.reload);
   watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
 function serveSass() {
-  return src('./sass/*.sass')
+  return src('./sass/**/*.sass', './sass/**/*.scss')
     .pipe(sass()) // запускаем sass
-    .pipe(dest(".css"))
+    .pipe(dest("css"))
     .pipe(browserSync.stream());
 };
 
