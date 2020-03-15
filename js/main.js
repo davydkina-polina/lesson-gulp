9a0fb98afc84a025719582d90be6283e24fb6c47
davydkina-polina <289199@mail.ru>
2020-03-15 14:37:34 +0300

    refact: modal close and open alright

--- js/main.js
+++ js/main.js
@@ -2,18 +2,27 @@ document.addEventListener("DOMContentLoaded", function(){
  const modal = document.querySelector(".modal");
  const modalBtn = document.querySelectorAll("[data-toggle=modal]");
  const closeBtn = document.querySelector(".modal__close");
- const closeModal = document.querySelector('.modal');
+ const btn = document.getElementById("myBtn");
  const switchModal = () => {
   modal.classList.toggle("modal--visible");
  }
-
  modalBtn.forEach(element => {
   element.addEventListener("click", switchModal);
  });
 
- closeBtn.addEventListener('click', switchModal);
+ closeBtn.onclick = function() {
+  modal.style.display = "none";
+ }
 
- closeModal.addEventListener('click', switchModal);
+ btn.onclick = function() {
+  modal.style.display = "flex";
+ }
+
+ window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+ }
 
  document.body.addEventListener('keyup', function (e) {
  var key = e.keyCode;
