5a98ab5592c85a8303d796e968d9351c293a5aa5
davydkina-polina <289199@mail.ru>
2020-03-20 00:44:33 +0300

    refact: minimize css in style.min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
-const {src, dest, watch} = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
-// const cleanCSS = require('gulp-clean-css');
-// const rename = require('gulp-rename');
+const {src, dest, watch, task} = require('gulp');
+const browserSync  = require('browser-sync').create();
+const sass         = require('gulp-sass'); // компилируем sass в css  
+ const autoprefixer = require('gulp-autoprefixer'); // добавляем к css префиксы для старых браузеров
+ const concatCss    = require('gulp-concat-css'); // объединяем все css файлы в один
+ const cleanCSS     = require('gulp-clean-css'); // с помощью этого будем добавлять к имени файла .min
+ const rename     = require('gulp-rename'); // с помощью этого будем переименовывать файлы
+ const del          = require('del'); // с помощью этого удалим предыдущий файл style.min.css
 
-// Static server
+
+// gulp.task('serve', ['sass'], function() {
+
+// browserSync.init({
+// server: "src/"
+// });
+
+// gulp.watch("src/sass/*.sass", ['sass']);
+// gulp.watch("src/*.html").on('change', browserSync.reload);
+// });   --- это для галп 3 версии. используем для 4 версии:
+
 function bs() {
-  serveSass();
+  deleteMinCss(); // запускаем функцию удаления предыдущего файла style.min.css -- важно, чтобы эта функция была первее остальных, а то не успеет удалиться файл
+  serveSass(); // запускаем функцию с sass
+  minimizeCss(); // запускаем функцию с минимизацией css
   browserSync.init({
       server: {
           baseDir: "./"
@@ -18,17 +33,60 @@ function bs() {
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
+/* gulp.task('sass', function() {
+
+return gulp.src("src/sass/*.sass")
+
+.pipe(sass().on('error', sass.logError))
+ .pipe(autoprefixer({
+ browsers: ['last 2 versions'],
+ cascade: false
+ }))
+ .pipe(concatCss("style.css"))
+.pipe(gulp.dest("src/css"))
+.pipe(browserSync.stream());
+
+}); --- это для галп 3 версии. используем для 4 версии: */
+
 function serveSass() {
   return src('./sass/**/*.sass', './sass/**/*.scss')
-    .pipe(sass()) // запускаем sass
+    .pipe(sass()) 
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(concatCss("style.css"))
     .pipe(dest("css"))
     .pipe(browserSync.stream());
 };
 
-exports.serve = bs;
+/*gulp.task('mincss', function() {
 
-// gulp.task('gulp-rename', () => {
-//   return gulp.src('styles/*.css')
-//     // .pipe(rename('.min.css'));
-//     .pipe(rename({ suffix: '.min', prefix : '' }))
-// })
+return gulp.src("src/css/*.css")
+
+.pipe(rename({suffix: ".min"}))
+.pipe(cleanCSS())
+.pipe(gulp.dest("app/css"));
+
+}); --- это для галп 3 версии. используем для 4 версии: */
+
+function minimizeCss() {
+  return src("./css/*.css")
+    .pipe(rename({suffix: ".min"}))
+    .pipe(cleanCSS())
+    .pipe(dest("./css"));
+};
+
+function deleteMinCss() {
+ del.sync('css/style.min.css')
+}; // функция, которая позволяет при последующем запуске gulp serve перезаписать новый файл style.min.css -- чтобы не создавалось style.min.min.css
+
+/* в 3 версии вызывается функция минимизации так:
+ gulp.task('min', ['mincss']);
+ 
+ в 4 версии так: 
+ exports.min = minimizeCss;  --- но мы вызываем эту функцию в bs */
+
+exports.serve = bs; // указываем название задачи для запуска - serve, указываем функцию, которую запускаем - bs
+/* gulp.task('default', ['serve']); */
+
+
--- js/main.js
+++ js/main.js
@@ -1,36 +1,73 @@
-document.addEventListener("DOMContentLoaded", function(){
- const modal = document.querySelector(".modal");
- const modalBtn = document.querySelectorAll("[data-toggle=modal]");
- const closeBtn = document.querySelector(".modal__close");
- const btn = document.getElementById("myBtn");
- const switchModal = () => {
-  modal.classList.toggle("modal--visible");
- }
- modalBtn.forEach(element => {
-  element.addEventListener("click", switchModal);
- });
+$(document).ready( function(){
+ var modal = $('.modal'),
+     modalBtn = $('[data-toggle=modal]'),
+     closeBtn = $('.modal__close');
+     btn = $('#myBtn');
 
- closeBtn.onclick = function() {
-  modal.style.display = "none";
- }
+     modalBtn.on('click', function () {
+      modal.toggleClass('modal--visible');
+     });
+     closeBtn.on('click', function () {
+      modal.toggleClass('modal--visible');
+      // modal.style.display = "none";
+     });
 
- btn.onclick = function() {
-  modal.style.display = "flex";
- }
+     $(document).keydown(function(e) {
+      // ESCAPE key pressed
+      if (e.keyCode == 27) {
+       modal.toggleClass('modal--visible');
+      }
+     });
+     
+     btn.on('click', function () {
+      modal.css("display", "flex");
+     });
 
- window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
- }
-
- document.body.addEventListener('keyup', function (e) {
- var key = e.keyCode;
-
- if (key == 27) {
-  modal.classList.toggle("modal--visible");
- };
- }, false);
-
+     window.on('click', function () {
+      // modal.css("display", "none");
+      modal.toggleClass('modal--visible');
+     });
+     
+  //    $(document).click(function(e){
+  //     if ($(e.target).parents().filter('.modal:visible').length != 1 && !$(e.target).hasClass('[data-toggle=modal]')) {
+  //         $('.modal').hide();
+  //     }
+  // });
 });
+
+// document.addEventListener("DOMContentLoaded", function(){
+//  const modal = document.querySelector(".modal");
+//  const modalBtn = document.querySelectorAll("[data-toggle=modal]");
+//  const closeBtn = document.querySelector(".modal__close");
+//  const btn = document.getElementById("myBtn");
+//  const switchModal = () => {
+//   modal.classList.toggle("modal--visible");
+//  }
+//  modalBtn.forEach(element => {
+//   element.addEventListener("click", switchModal);
+//  });
+
+//  closeBtn.onclick = function() {
+//   modal.style.display = "none";
+//  }
+
+//  btn.onclick = function() {
+//   modal.style.display = "flex";
+//  }
+
+//  window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+//  }
+
+//  document.body.addEventListener('keyup', function (e) {
+//  var key = e.keyCode;
+
+//  if (key == 27) {
+//   modal.classList.toggle("modal--visible");
+//  };
+//  }, false);
+
+// });
 
